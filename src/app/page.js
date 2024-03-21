f5916537f4c48e7d331db85b5f5cc8b24bd7379f
sharif-github <107545661+sharif-github@users.noreply.github.com>
2024-03-21 18:57:44 +0600

    homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
+import LatestNews from "@/components/ui/LatestNews";
+import World from "@/components/ui/World";
+
 
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>HOME PAGE</h1>
+    <div className="grid grid-cols-12 px-28 my-5 gap-5">
+      <div className="col-span-8">
+        <h1 className="text-xl font-semibold text-gray-600 mt-3 mb-5 border-b-4 pl-8 pb-2 relative">Latest News
+          <div className="border-b-4 border-rose-400 w-48 absolute -bottom-1 left-0"></div>
+        </h1>
+        <LatestNews></LatestNews>
+      </div>
+      <div className="col-span-4">
+        <h1 className="text-xl font-semibold text-gray-600 mt-3 mb-5 border-b-4 pl-8 pb-2 relative">World
+          <div className="border-b-4 border-rose-400 w-36 absolute -bottom-1 left-0"></div>
+        </h1>
+        <World></World>
+      </div>
     </div>
   );
 };
---
+++ src/components/ui/World.js
@@ -0,0 +1,31 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import bitcoin from '@/assets/bitcoin1.jpg';
+import Image from 'next/image';
+
+const World = () => {
+    return (
+        <div>
+            <Card>
+                <CardActionArea>
+                    <CardMedia>
+                        <Image src={bitcoin} alt=''></Image>
+                    </CardMedia>
+                    <CardContent>
+                        <Typography gutterBottom>
+                            Cryptocurrency Price on March 20: Bitcoin tumbles 6% to below $61,000 ahead of Fed meet outcome
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Bitcoin fell 6.4% to $60,969 on Wednesday, hitting its two-week low ahead of the Federal Reserve's policy decision and commentary later in the day. Concurrently, Ethereum, the second-largest cryptocurrency, also saw a decline of 9%, falling to $3,075.
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </div>
+    );
+};
+
+export default World;
